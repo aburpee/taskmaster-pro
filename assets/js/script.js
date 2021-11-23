fa8ec0aa53a6dc79239a0943daec94f25a78271a
bonobo <arburpee@gmail.com>
2021-11-22 22:43:50 -0800

    added drag and drop, remove, and persistent status

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,59 @@
 var tasks = {};
 
+//drag and drop and save section on refresh
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false, 
+  tolerance: "pointer", 
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this)
+  },
+  over: function(event) {
+    console.log("over", event.target)
+  },
+  out: function(event) {
+    console.log("out", event.target)
+  },
+  update: function(event) {
+    var tempArr = [];
+
+    $(this).children().each(function() {
+    var text = $(this).find("p").text().trim();
+    var date = $(this).find("span").text().trim();
+    tempArr.push({
+      text:text, 
+      date: date
+    });
+  });
+  // console.log(tempArr);
+  var arrName = $(this).attr("id").replace("list-", "");
+  tasks[arrName] = tempArr;
+  saveTasks();
+  
+}
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+
+})
+
+
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
